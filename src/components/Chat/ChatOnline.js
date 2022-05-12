b82eff7d8f94ab6f142b1a036d42f1ed0e3fed1b
Nguyen Thi Tra <lionelnguyen1998@gmail.com>
2022-05-12 10:20:41 +0700

    fixbug_chat

--- src/components/Chat/ChatOnline.js
+++ src/components/Chat/ChatOnline.js
@@ -5,6 +5,7 @@ import "./chatOnline.css";
 export default function ChatOnline({ onlineUsers, currentId, setCurrentChat }) {
     const [users, setUsers] = useState([]);
     const [onlineFriends, setOnlineFriends] = useState([]);
+    const [query, setQuery] = useState([])
 
     useEffect(() => {
         chatApi.getAllUsers()
@@ -25,18 +26,33 @@ export default function ChatOnline({ onlineUsers, currentId, setCurrentChat }) {
             })
             .catch(e => console.log(e))
     }
+    console.log(onlineFriends)
     return (
         <>
+            <input 
+                type="text"
+                placeholder="検索" 
+                className="chatMenuInput" 
+                onChange={(e) => setQuery(e.target.value)}
+            />
             {
-                onlineFriends.map((o, index) => (
+                onlineFriends.filter(onlF => 
+                onlF.name.toLowerCase().includes(query))
+                .map((o, index) => (
                     <div className="chatOnlineFriend" key={index} onClick={() => {handleClick(o)}}>
                         <div className="chatOnlineImgContainer">
-                        <img
-                            className="chatOnlineImg"
-                            src={o ? o.avatar : ''}
-                            alt=""
-                        />
-                        <div className="chatOnlineBadge"></div>
+                            <img
+                                className="chatOnlineImg"
+                                src={o ? o.avatar : ''}
+                                alt=""
+                            />
+                            {
+                                o.role === 2 ? (
+                                    <div className="chatOnlineBadge"></div>
+                                ) : (
+                                    <div className="chatOnlineBadgeAdmin"><img className="adminStar" src='https://freesvg.org/img/1289679474.png' alt='admin'/></div>
+                                )
+                            }
                         </div>
                         <span className="chatOnlineName">{o ? o.name : ''}</span>
                     </div>
--- src/components/Chat/Conversation.js
+++ src/components/Chat/Conversation.js
@@ -3,14 +3,22 @@ import "./conversation.css";
 
 export default function Conversation({conversation}) {
   const userReceive = conversation.user_receive_info;
+  console.log(userReceive)
   return (
     <div className="conversation">
-      <img
-        className="conversationImg"
-        src={userReceive ? userReceive.avatar : `http://img.phebinhvanhoc.com.vn/wp-content/uploads/2021/07/imager_1_2269_700-1.jpg`}
-        alt={userReceive ? userReceive.name : ''}
-      />
-      <span className="conversationName">{userReceive ? userReceive.name : ''}</span>
+      <div className="chatOnlineImgContainer">
+        <img
+          className="conversationImg"
+          src={userReceive ? userReceive.avatar : `http://img.phebinhvanhoc.com.vn/wp-content/uploads/2021/07/imager_1_2269_700-1.jpg`}
+          alt={userReceive ? userReceive.name : ''}
+        />
+        { 
+          userReceive.role === 1 && (
+              <div className="chatOnlineBadgeAdminConv"><img className="adminStarConv" src='https://freesvg.org/img/1289679474.png' alt='admin'/></div>
+          )
+        }
+        <span className="conversationName">{userReceive ? userReceive.name : ''}</span>
+      </div>
     </div>
   );
 }
